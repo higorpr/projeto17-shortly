0144a41a76914f2f93d5be76eff19c46b42c4939
higorpr <higorpr@gmail.com>
2022-12-15 02:37:13 -0300

    reformat: set up environment

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, console.log(`App server running on port ${port}`));
